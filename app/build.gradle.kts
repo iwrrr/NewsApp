b0765d18cbed6c1899d0e6ceac16bbcc2c001fc8
Irya <iryadev15@gmail.com>
2025-07-24 03:29:24 +0700

    feat: add Paging dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,9 +67,11 @@ dependencies {
 
     implementation(libs.koin.androidx.compose)
 
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.paging)
     ksp(libs.androidx.room.compiler)
 
     implementation(libs.androidx.navigation.compose)
