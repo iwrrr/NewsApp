c7215a5172e8ad6266410ab05a35b431e8a937f4
Irya <iryadev15@gmail.com>
2025-07-24 03:29:57 +0700

    feat: add searchNews function

--- app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -1,13 +1,14 @@
 package com.example.newsapp.domain.repository
 
+import androidx.paging.PagingData
 import com.example.newsapp.domain.model.News
 import com.example.newsapp.utils.state.DataState
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
-    suspend fun getNews(
-        query: String? = null,
-        page: Int? = null,
-        pageSize: Int? = null
-    ): Flow<DataState<List<News>>>
+    fun getNews(): Flow<DataState<List<News>>>
+
+    fun searchNews(
+        query: String,
+    ): Flow<PagingData<News>>
 }
