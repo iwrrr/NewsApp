2337bcd72e6aedb718ae006c9bf408140f5921d7
Irya <iryadev15@gmail.com>
2025-07-24 00:39:46 +0700

    feat: create News data class

---
+++ app/src/main/java/com/example/newsapp/domain/News.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.domain
+
+data class News(
+    val id: Int,
+    val title: String,
+    val author: String,
+    val publishedAt: String,
+    val imageUrl: String,
+    val url: String,
+)
